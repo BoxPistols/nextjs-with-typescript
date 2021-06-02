4b42a0849b8452b3c21a52baafde4d18156f4740
ait <ait@ex.in>
2021-06-02 20:56:25 +0900

    CSS Env / Css-Modules

--- pages/materialUi/index.tsx
+++ pages/materialUi/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Link from '../../src/Link'
 // import Mui_Dialog from '/pages//materialUi/dialog'
+
 // css
 import css from '../styles/base.scss'
 
-export default function index() {
+export default function index () {
     return (
         <div className={css.example}>
-            <h2>Mui_Dialog </h2>
+            <h2>Mui_Dialog</h2>
             {/* <Mui_Dialog /> */}
             <Link href='/materialUi/dialog' color='secondary'>
                 Go to the Dialog
